89da3b0914e0dba08423f5939a70f99af4436951
ChenYaoGang <chenyg@uniplore.io>
2023-04-12 19:04:15 +0800

    支持设置角色

--- OpenFlow/src/UniploreBridge.ts
+++ OpenFlow/src/UniploreBridge.ts
@@ -36,8 +36,8 @@ export class UniploreBridge{
 
                 try{
                     switch(action){
-                        case "ensureAdmin":
-                            await this.ensureAdmin(req, res, span);
+                        case "ensureUser":
+                            await this.ensureUser(req, res, span);
                             break;
                         default: //unknown
                         this.response(res, {code: 404, msg: "unknown action: " + action});
@@ -65,9 +65,8 @@ export class UniploreBridge{
     }
 
     //参考：LoginProvider.CreateLocalStrategy
-    //在iDIS里，首次需要使用管理员帐号访问，以完成OpenFlow的初始化
-    //注意：此接口必须是iDIS的管理员调用
-    private async ensureAdmin(req, res, span: Span) {
+    //在iDIS里，首次需要使用初始管理员帐号访问，以完成OpenFlow的初始化
+    private async ensureUser(req, res, span: Span) {
         //注意name、username不能是openflow保留的名称：如root,admin,administrator等
 
         const params=req.body;
@@ -77,6 +76,7 @@ export class UniploreBridge{
 
         let name: string = params.name || params.username;
         let username: string = params.username;
+        const roles: string[] = params.roles ||[];
 
         const clientagent = params.clientagent;
         const clientversion = params.clientversion;
@@ -97,6 +97,7 @@ export class UniploreBridge{
             remoteip = LoginProvider.remoteip(req);
         }
 
+        const rootJwt: string = Crypt.rootToken();
 
         let user: User = await Logger.DBHelper.FindByUsername(username, null, span);
         const providers = await Logger.DBHelper.GetProviders(span);
@@ -112,13 +113,13 @@ export class UniploreBridge{
                 Logger.instanse.info("No login providers, creating " + username + " as admin", span);
                 user = new User(); user.name = name; user.username = username;
                 //await Crypt.SetPassword(user, password, span);
-                const jwt: string = Crypt.rootToken();
-                user = await Logger.DBHelper.EnsureUser(jwt, user.name, user.username, null, devPassword ? devPassword : password, null, span);
+                //const jwt: string = Crypt.rootToken();
+                user = await Logger.DBHelper.EnsureUser(rootJwt, user.name, user.username, null, devPassword ? devPassword : password, null, span);
 
                 const admins: Role = await Logger.DBHelper.FindRoleByName("admins", null, span);
                 if (admins == null) throw new Error("Failed locating admins role!")
                 admins.AddMember(user);
-                await Logger.DBHelper.Save(admins, Crypt.rootToken(), span)
+                await Logger.DBHelper.Save(admins, rootJwt, span)
             }else{
                 // if (!(await Crypt.ValidatePassword(user, password, span))) {
                 //     Logger.instanse.error("No login providers, login for " + username + " failed", span);
@@ -130,7 +131,7 @@ export class UniploreBridge{
                 const admins: Role = await Logger.DBHelper.FindRoleByName("admins", null, span);
                 if (admins == null) throw new Error("Failed locating admins role!")
                 admins.AddMember(user);
-                await Logger.DBHelper.Save(admins, Crypt.rootToken(), span)
+                await Logger.DBHelper.Save(admins, rootJwt, span)
             }
 
             Logger.instanse.info("Clear cache", span);
@@ -138,38 +139,70 @@ export class UniploreBridge{
             await Audit.LoginSuccess(TokenUser.From(user), "local", "local", remoteip, "browser", "unknown", span);
             const provider: Provider = new Provider(); provider.provider = "local"; provider.name = "Local";
             Logger.instanse.info("Saving local provider", span);
-            const result = await Config.db.InsertOne(provider, "config", 0, false, Crypt.rootToken(), span);
+            const result = await Config.db.InsertOne(provider, "config", 0, false, rootJwt, span);
             Logger.instanse.info("local provider created as " + result._id, span);
             await Logger.DBHelper.CheckCache("config", result, false, false, span);
         }else if(user==null){//用户不存在，则创建
-            const rootJwt: string = Crypt.rootToken();
             user = new User(); user.name = name; user.username = username;
             user = await Logger.DBHelper.EnsureUser(rootJwt, user.name, user.username, null, password, null, span);
         }else if(user.name!==name || user.impersonating){
-           const rootJwt: string = Crypt.rootToken();
             user.name=name;
             user.impersonating='';//设置为空，否则无意开启饰演某个角色、导致数据混乱：使用A帐号登录，但实际更改的数据都是B的
             await Logger.DBHelper.Save(user,rootJwt,span);
         }
 
-        //判断是否为管理员，若不是、则设置管理员角色
-        const admins: Role = await Logger.DBHelper.FindRoleByName("admins", null, span);
-        if (admins == null) throw new Error("Failed locating admins role!")
+        if(isUniploreAdmin){//系统管理员始终添加admins角色
+            roles.push("admins");
+        }
 
-        if (!admins.IsMember(user._id)) {
-            const rootJwt: string = Crypt.rootToken();
-            admins.AddMember(user);
-            await Logger.DBHelper.Save(admins, rootJwt, span);
+        ////////////////////////////////
+        //角色处理
+        ///////////////////////////////
+        //1.获取当前用户实际拥有的角色
+        const currentRolesResult = await Config.db.query({ query: {
+            "$and": [
+                {"_type": "role"},
+                { "members": { "$elemMatch": { "_id": user._id } } },
+            ]
+        }, skip: 0, top: 100, collectionname: "users", jwt: rootJwt }, span);
+
+        //2.获取需要移除、新添加的角色
+        const needRemoveRoles: string[] = [];
+        for(let roleItem of currentRolesResult){
+            let index = roles.indexOf(roleItem.name);
+            if(index==-1){
+                needRemoveRoles.push(roleItem.name);
+            }else{
+                roles.splice(index,1); //已经存在的、移除，剩下的就是新增的
+            }
+        }
+
+        //3.移除已取消的角色
+        for(let rolename of needRemoveRoles){
+            const roleObject: Role = await Logger.DBHelper.FindRoleByName(rolename, null, span);
+            roleObject.RemoveMember(user._id);
+            await Logger.DBHelper.Save(roleObject, rootJwt, span);
+        }
+
+        //4.新增角色处理
+        for(let rolename of roles){
+            const roleObject: Role = await Logger.DBHelper.FindRoleByName(rolename, null, span);
+            //if (roleObject == null) throw new Error(`Failed locating ${rolename} role!`)
+
+            if (!roleObject.IsMember(user._id)) {
+                roleObject.AddMember(user);
+                await Logger.DBHelper.Save(roleObject, rootJwt, span);
+            }
         }
 
         const jwt = Crypt.createToken(user, Config.shorttoken_expires_in);
 
         await Audit.LoginSuccess(TokenUser.From(user), "local", "local", remoteip, clientagent, clientversion, span);
 
-        this.response(res, { code: 200, data: {jwt} });
+        this.response(res, { code: 200, data: {jwt,_id: user._id} });
     }
 
-    private response(res, result){
+    private response(res: any, result: any){
         res.json(result);
     }
 
